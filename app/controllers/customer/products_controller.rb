6c62c351eeaa7725387c5bffdd6cceaed7b63437
natsumi <natsu.bsk810@gmail.com>
2024-06-13 22:12:57 +0900

    Implementation on the customer's side of the product

---
+++ app/controllers/customer/products_controller.rb
@@ -0,0 +1,23 @@
+class Customer::ProductsController < ApplicationController
+  def index
+    @products, @sort = get_products(params)
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  private
+
+  def get_products(params)
+    if params[:latest]
+      [Product.latest, 'latest']
+    elsif params[:price_high_to_low]
+      [Product.price_high_to_low, 'price_high_to_low']
+    elsif params[:price_low_to_high]
+      [Product.price_low_to_high, 'price_low_to_high']
+    else
+      [Product.all, 'default']
+    end
+  end
+end
